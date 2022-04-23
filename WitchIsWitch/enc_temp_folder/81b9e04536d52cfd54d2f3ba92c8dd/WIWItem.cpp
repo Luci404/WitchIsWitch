dad0713e650cb0da2419cc2672348d9ed7826784
stormsoendergaard@gmail.com <stormsoendergaard@gmail.com>
2022-04-23 18:19:44 +0200

    Interact fixes

--- WitchIsWitch/enc_temp_folder/81b9e04536d52cfd54d2f3ba92c8dd/WIWItem.cpp
+++ WitchIsWitch/enc_temp_folder/81b9e04536d52cfd54d2f3ba92c8dd/WIWItem.cpp
@@ -9,8 +9,6 @@ AWIWItem::AWIWItem()
 	PrimaryActorTick.bCanEverTick = false;
 
 	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
-	Mesh->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
-	Mesh->SetCollisionResponseToChannel(ECollisionChannel::ECC_GameTraceChannel1, ECollisionResponse::ECR_Block);
 	RootComponent = Mesh;
 
 	bReplicates = true;
@@ -30,8 +28,6 @@ void AWIWItem::BeginPlay()
 // Pickup
 void AWIWItem::Pickup_Implementation(AWIWCharacter* interactor)
 {
-	Mesh->SetVisibility(!interactor->IsLocallyControlled());
-	Mesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 	m_Interactor = interactor;
 	m_Interactor->HeldItem = this;
 	RootComponent->AttachToComponent(m_Interactor->HandLocator, FAttachmentTransformRules::SnapToTargetIncludingScale);
@@ -43,8 +39,6 @@ void AWIWItem::Drop_Implementation()
 	RootComponent->DetachFromComponent(FDetachmentTransformRules::KeepWorldTransform);
 	m_Interactor->HeldItem = nullptr;
 	m_Interactor = nullptr;
-	Mesh->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
-	Mesh->SetVisibility(true);
 }
 
 // Reset
